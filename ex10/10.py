f3672972f87ed1917a31c2aa6680096243de35ff
Victor Pereira <vpereirabr@gmail.com>
2025-01-26 17:07:10 +0100

    tidy up ex10

--- ex10/10.py
+++ ex10/10.py
@@ -1,10 +1,15 @@
+"""
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+Find the sum of all the primes below two million.
+"""
+
 limitNumber = 2000000
 primes = [2]
 
 
 def isPrime(n):
     # instead of call math.sqrt we can simply **.5 which does the same but its cheaper
-    for i in range(2, int(n**0.5) + 1):
+    for i in range(3, int(n**0.5) + 1, 2):
         if n % i == 0:
             return False
     return True
@@ -13,7 +18,5 @@ def isPrime(n):
 for currentNumber in range(3, limitNumber + 1, 2):
     if isPrime(currentNumber):
         primes.append(currentNumber)
-    print(f"currentNumber {currentNumber}")
-    print(f"primes len = {len(primes)}")
 
-print(f"The sum of the  primes under {limitNumber} is {sum(primes)}")
+print(f"The sum of the  primes under {limitNumber} is {sum(primes)}")  # 142913828922
