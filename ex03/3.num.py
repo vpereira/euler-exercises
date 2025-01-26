25f7b28995c34e970a760560b56f2d2daaf52690
Victor Pereira <vpereirabr@gmail.com>
2025-01-26 16:44:22 +0100

    Black it

--- ex03/3.num.py
+++ ex03/3.num.py
@@ -2,10 +2,12 @@
     The prime factors of 13195 are 5, 7, 13 and 29.
     What is the largest prime factor of the number 600851475143 ?
 """
+
 import numpy as np
 
 numberToFactor = 600851475143
 # numberToFactor = 13195
+
 
 def isPrime(n):
     if n < 2:
@@ -25,14 +27,15 @@ def isPrime(n):
 
     return True
 
+
 def factors(n):
     _factors = []
-    
+
     # Handle even numbers using NumPy
     while n % 2 == 0:
         _factors.append(2)
         n = n // 2  # Integer division flooring the result
-    
+
     # Handle odd numbers using NumPy arrays
     potential_factors = np.arange(3, int(np.sqrt(n)) + 1, 2)
     for i in potential_factors:
@@ -45,7 +48,8 @@ def factors(n):
 
     if n > 2:
         _factors.append(int(n))  # Append the remaining prime factor
-    
+
     return np.sort(_factors)
+
 
 print(factors(numberToFactor)[-1])  # Output: 6857
--- ex03/3.py
+++ ex03/3.py
@@ -6,6 +6,7 @@
 numberToFactor = 600851475143
 # numberToFactor = 13195
 
+
 def isPrime(n):
     if n < 2:
         return False
@@ -21,14 +22,15 @@ def isPrime(n):
 
     return True
 
+
 def factors(n):
     _factors = []
-    
+
     # handle even numbers
     while n % 2 == 0:
         _factors.append(2)
-        n = n // 2 # integer division flooring the result
-    
+        n = n // 2  # integer division flooring the result
+
     # handle odd numbers
     for i in range(3, int(n**0.5) + 1, 2):
         while n % i == 0:
