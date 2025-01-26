d733a5e5dc0a61f6a08072202f024e5dba6cbb98
Victor Pereira <vpereirabr@gmail.com>
2025-01-26 15:56:25 +0100

    Add numpy version for ex01 and ex02

--- ex01/1.py
+++ ex01/1.py
@@ -1,7 +1,8 @@
-limitNumber = 1000
-results = 0
+"""
+	If we list all the natural numbers below 10 that are multiples of 3 or 5,
+	we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-for i in range(1, limitNumber):
-    if i % 3 == 0 or i % 5 == 0:
-        results = results + i
-print(f"results = {results}")
+	Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+limitNumber = 1000
+print(sum([i for i in range(1, limitNumber) if i % 3 == 0 or i % 5 == 0])) # 233168
